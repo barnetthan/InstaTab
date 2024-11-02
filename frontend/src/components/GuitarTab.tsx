1652f24be8cf37e5fd52c6dcdb77fcccb6daf0db
barnetthan <barnett.han@gmail.com>
2024-11-02 17:18:23 -0500

    Transforming songs into guitar tab form

---
+++ frontend/src/components/GuitarTab.tsx
@@ -0,0 +1,63 @@
+import { ReactElement } from "react";
+import { Song, Tab } from "../pages/HomePage.tsx";
+import "../styles/Tabs.css";
+
+interface GuitarTabProps {
+  song: Song;
+}
+
+function GuitarTab({ song }: GuitarTabProps) {
+  function createTabs() {
+    const arr: ReactElement[] = [];
+    const strings: string[] = ["E", "B", "G", "D", "A", "E"];
+    const maxNotesPerLine = 25;
+
+    // Create the base line with dashes
+    let line = "";
+    for (let i = 0; i <= song.maxTime; i++) {
+      line += "―";
+    }
+
+    let curIndex = 0;
+    let lines: string[] = [];
+
+    // Generate lines for each string
+    for (let i = 0; i < 6; i++) {
+      let curLine = line;
+
+      // Place frets on the correct positions in the line for each tab
+      while (curIndex < song.tabs.length && song.tabs[curIndex].string === i) {
+        const curTab: Tab = song.tabs[curIndex];
+        curLine = curLine.substring(0, curTab.time) + curTab.fret + curLine.substring(curTab.time + 1);
+        curIndex++;
+      }
+      lines.push(curLine);
+    }
+
+    // Chunk each string line into segments of 25 characters
+    for (let chunkStart = 0; chunkStart < song.maxTime; chunkStart += maxNotesPerLine) {
+      for (let i = 0; i < 6; i++) {
+        const curLineChunk = lines[i].substring(chunkStart, chunkStart + maxNotesPerLine);
+        const lineWithStringMarker = strings[i] + " " + curLineChunk;
+
+        // Render each character in the line as a span element
+        for (let j = 0; j < lineWithStringMarker.length; j++) {
+          arr.push(<span className="tabBox" key={`${i}-${chunkStart}-${j}`}>{lineWithStringMarker[j]}</span>);
+        }
+        arr.push(<br key={`linebreak-${i}-${chunkStart}`} />); // Break for each string line
+      }
+      arr.push(<br/>);
+      arr.push(<br key={`sectionbreak-${chunkStart}`} />); // Add an extra break between sections of 25 notes
+    }
+
+    return arr;
+  }
+
+  return (
+    <>
+      {createTabs()}
+    </>
+  );
+}
+
+export default GuitarTab;
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,8 +1,37 @@
 import NavBar from "../components/NavBar";
 import { useState } from "react";
+import GuitarTab from "../components/GuitarTab";
+
+export interface Song {
+  tabs: Tab[];
+  favorite: boolean;
+  maxTime: number;
+}
+
+export interface Tab {
+  string: number;
+  time: number;
+  fret: number;
+}
 
 function HomePage() {
   const [link, setLink] = useState<string>("");
+
+  // Dummy data for testing
+  const dummyTabs: Tab[] = [
+    { string: 0, time: 77, fret: 3 },
+    { string: 1, time: 3, fret: 5 },
+    { string: 1, time: 2, fret: 0 },
+    { string: 4, time: 1, fret: 2 },
+    { string: 4, time: 0, fret: 3 },
+    { string: 5, time: 5, fret: 0 },
+  ];
+
+  const dummySong: Song = {
+    tabs: dummyTabs,
+    favorite: false,
+    maxTime: 80,
+  };
 
   function handleSubmit() {
     // implicity validates the input
@@ -10,7 +39,7 @@ function HomePage() {
 
     // call api endpoint with current link
     // ex: axios.get(`https://api.example.com/getdata/${link}`);
-    
+
     setLink("");
   }
 
@@ -35,7 +64,9 @@ function HomePage() {
         <button disabled={!link} onClick={handleSubmit}>
           Submit
         </button>
+        <br/>
       </form>
+      <GuitarTab song={dummySong} />
     </>
   );
 }
